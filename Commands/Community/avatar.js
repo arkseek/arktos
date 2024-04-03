12bd65fb1cd68bca8bb7279065162f5423d2d92e
Hamza Elashry <98109755+arkseek@users.noreply.github.com>
2024-04-03 01:35:45 -0400

    Add files via upload

--- Commands/Community/avatar.js
+++ Commands/Community/avatar.js
@@ -3,16 +3,17 @@ const { SlashCommandBuilder, AttachmentBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("user-info")
-    .setDescription("get the info of a user")
+    .setName("avatar")
+    .setDescription("get the avatar of a user")
     .addUserOption(option =>(
-      option.setName('target').setDescription('the info of the user you want to fetch').setRequired(true))),
+      option.setName('target').setDescription('the pfp of the user you want to fetch').setRequired(true))),
   run: ({ interaction, client, handler }) => {
     const user = interaction.options.getUser('target')
 
     const embed = new EmbedBuilder()
-      .setTitle(`Information of ${user.username}`)
-      .setDescription(` \*\*ID\*\*: ${user.id} \n \*\*Profile\*\*: \`\`\`${user.avatarURL()}\`\`\``)
+      .setTitle(`Avatar of ${user.tag}`)
+      .setImage(user.avatarURL())
+
     interaction.reply({ embeds: [embed] });
   },
   options: {},
--- Commands/Moderation/kick.js
+++ Commands/Moderation/kick.js
@@ -2,30 +2,28 @@ const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("disc
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('ban')
-        .setDescription('ban a user')
+        .setName('kick')
+        .setDescription('kick a user')
         .addUserOption(option =>(
-            option.setName('target').setDescription('the user you want to ban').setRequired(true)
+            option.setName('target').setDescription('the user you want to kick').setRequired(true)
         ))
         .addStringOption(option => (
-            option.setName('reason').setDescription('the reason for the ban.')
+            option.setName('reason').setDescription('the reason for the kick.').setRequired(false)
         ))
-        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
     run: ({ interaction, client, handler}) => {
         const target = interaction.options.getUser('target')
         const reasoning = interaction.options.getString('reason') ?? 'no reason was provided.'
 
         const embed = new EmbedBuilder()
-            .setTitle(`Banned ${target}`)
-            .setDescription(`${interaction.user.username} banned \*\*\*${target}\*\*\* \n Reason: \`\`\`${reasoning}\`\`\``)
+            .setTitle(`Kicked ${target}`)
+            .setDescription(`${interaction.user.username} Kicked \*\*\*${target}\*\*\* \n Reason: \`\`\`${reasoning}\`\`\``)
             .setColor("#df2935")
             .setTimestamp()
         
         interaction.reply({ embeds: [embed], ephemeral: true })
-        interaction.guild.members.ban(target)
+        interaction.guild.members.kick(target)
     },
     option: {
-        userPermissions: ['Administrator', 'BanMembers'],
-        botPermissions: ['Administrator', 'BanMembers']
     }
 }
